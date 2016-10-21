de125f9f28c8c0f441fde8be622204bf63024238
Juan Pablo Brenes <juanp1995@gmail.com>
2016-10-20 23:02:03 -0600

    Nuevas tablas en el script de construcción

---
+++ Procedimiento - asociarEtapa.sql
@@ -0,0 +1,54 @@
+﻿------------------------------------------------------------------------
+--			PROCEDIMIENTO ALMACENADO
+
+-- Permite asociar una etapa con un proyecto, agregando sus detalles como,
+-- la fecha de inicio y la fecha de finalización
+------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION asociarEtapa(
+	pIdProyecto INT, pIdEtapa INT, 
+	pFInicio DATE, pFFin DATE
+) RETURNS TEXT
+
+AS $$
+
+DECLARE 
+	_idRelacion INT;
+	_idDetalle INT;
+
+BEGIN
+
+	IF EXISTS (SELECT Id FROM PROYECTO WHERE Id=pIdProyecto)
+	THEN
+		IF EXISTS (SELECT Id FROM ETAPA WHERE Id=pIdEtapa)
+		THEN
+			INSERT INTO ETAPA_PROYECTO(Id_Proyecto, Id_Etapa)-- Se relaciona el proyecto con la etapa
+			VALUES (pIdProyecto, pIdEtapa)
+			RETURNING Id
+			INTO _idRelacion;
+
+			INSERT INTO DETALLE(Estado, F_Inicio, F_Fin)-- Se insertan los detalles de la etapa
+			VALUES (TRUE, pFInicio, pFFin)
+			RETURNING Id
+			INTO _idDetalle;
+
+			INSERT INTO DETALLE_ETAPA(Id_EtapaProyecto, Id_Detalle)-- Se asocia los detalles con la relacion proyecto-etapa
+			VALUES (_idRelacion, _idDetalle);
+
+			RETURN 'Etapa asociada correctamente';
+		ELSE
+			RETURN 'La etapa a asociar no existe';
+		END IF;
+		
+	ELSE
+		RETURN 'El proyecto asociado no existe';
+	END IF;
+
+END;
+	$$
+	LANGUAGE 'plpgsql' VOLATILE;
+
+select * from registrarproyecto('proyecto1', 'Cartago', 'El Guarco', 'San Isidro', 'juanp1995', 'juanp1995')
+select * from registraretapa('Etapa1', 'Descripcion')
+select * from asociarEtapa(1,1,'10/12/2016', '10/12/2017')
---
+++ Procedimiento - registrarEtapa.sql
@@ -0,0 +1,32 @@
+﻿------------------------------------------------------------------------
+--			PROCEDIMIENTO ALMACENADO
+
+-- Permite registrar una nueva etapa en la base de datos, la cual estara
+-- disponible para que cualquier ingeniero/arquitecto la asocie con sus
+-- proyectos
+------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION registrarEtapa(
+	pNombre TEXT, pDescripcion TEXT
+) RETURNS TEXT
+
+AS $$
+
+BEGIN
+
+	IF EXISTS (SELECT Nombre FROM ETAPA WHERE Nombre=pNombre)
+	THEN
+		RETURN 'Ya hay una etapa con el mismo nombre';
+	ELSE
+		INSERT INTO ETAPA(Nombre, Descripcion)
+		VALUES (pNombre, pDescripcion);
+
+		RETURN 'Etapa registrada correctamente';
+	END IF;
+
+END;
+	$$
+	LANGUAGE 'plpgsql' VOLATILE;
+
+
+select * from registraretapa('prueba2', 'etapaprueba')
