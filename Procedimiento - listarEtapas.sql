5996973af280a3b57e51c35584c54b238607912f
Juan Pablo Brenes <juanp1995@gmail.com>
2016-10-23 17:32:24 -0600

    Procedimiento para obtener las etapas del sistema

---
+++ Procedimiento - listarEtapas.sql
@@ -0,0 +1,22 @@
+﻿------------------------------------------------------------------------
+--			PROCEDIMIENTO ALMACENADO
+
+-- Permite obtener la lista de etapas registradas en la base de datos
+------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION listarEtapas() RETURNS TABLE(id INT, nombre VARCHAR(15), descripcion VARCHAR(100))
+
+AS $$
+
+BEGIN
+
+	RETURN QUERY SELECT et.Id, et.Nombre, et.Descripcion FROM ETAPA AS et;
+
+
+END;
+	$$
+	LANGUAGE 'plpgsql' VOLATILE;
+
+
+
+select * from listarEtapas()
